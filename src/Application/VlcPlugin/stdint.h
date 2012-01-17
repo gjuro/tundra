5ce135a4bb6b3d98107d16b1bd18d57722bc21c9
Cvetan Stefanovski <cvetan.stefanovski@itumtech.com>
2012-01-17 13:40:06 +0100

    VlcPlugin - stdint.h: override this file on Mac to the system default 'stdint.h' to avoid build errors

--- src/Application/VlcPlugin/stdint.h
+++ src/Application/VlcPlugin/stdint.h
@@ -2,6 +2,10 @@
 // THIS HEADER IS HERE ONLY FOR VLC INTERNALS THAT NEED STDINT.H.
 // DO NOT INCLUDE THIS ANYWHERE IN TUNDRA CODE SO YOU WONT GET THE USING BOOST:: DEFINES.
 
+#ifdef __APPLE__
+#include "/usr/include/stdint.h"
+#else
+
 #include "CoreTypes.h"
 
 #if (__STDC_VERSION__ >= 199901L) || (_MSC_VER >= 1600)
@@ -17,3 +21,5 @@
     using boost::int32_t;
     using boost::int64_t;
 #endif
+
+#endif // #ifdef __APPLE__
